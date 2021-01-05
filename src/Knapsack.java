c12c038d5c806f683f6f8c2593b718854ce2e49d
sheet-p <sheetal.mec@gmail.com>
2021-01-05 23:54:29 +0530

    Memoized solution

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,9 +1,31 @@
+import java.util.Arrays;
+
 public class Knapsack {
     /* This is a 0-1 Knapsack problem. We are given a knapsack and its total weight capacity. We need to figure the
     number of items to be put into this knapsack so that the profit is maximised. As input, we will be given the total
      weight capacity of the knapsack, the weights of individual items, the value of those individual items.
      */
     public static void main(String[] args) {
+        int weight[] ={1, 3, 4, 5}, val[]= {1, 4, 5, 7}, W = 0;
+        int len = val.length;
+        System.out.println(knapsack(weight, val, W, len));
+    }
+
+    public static int knapsack(int[] weight, int[] val, int W, int len) {
+        int maxPrice[][] = new int [len][W];
+        Arrays.fill(maxPrice, -1);
+        if(len==0 || W==0)
+            return 0;
+        if(maxPrice[len][W] != -1)
+            return maxPrice[len][W];
+        if(val[len-1] > W) {
+            return maxPrice[len][W] = knapsack(weight, val, W, len-1);
+        }
+
+
+            return maxPrice[len][W] = Math.max(val[len-1] + knapsack(weight, val, W-val[len-1], len-1) ,
+                                                    knapsack(weight, val, W, len-1))  ;
+
 
     }
 }
