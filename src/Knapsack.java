2d52d4f9d637e47c3275036433a72af5e36ad887
sheet-p <sheetal.mec@gmail.com>
2021-01-05 23:26:29 +0530

    Start of the Knapsack 0-1 problem

---
+++ src/Knapsack.java
@@ -0,0 +1,9 @@
+public class Knapsack {
+    /* This is a 0-1 Knapsack problem. We are given a knapsack and its total weight capacity. We need to figure the
+    number of items to be put into this knapsack so that the profit is maximised. As input, we will be given the total
+     weight capacity of the knapsack, the weights of individual items, the value of those individual items.
+     */
+    public static void main(String[] args) {
+
+    }
+}
