62503e38fcb9456423f9abba3f2543ab840d2b54
sheet-p <sheetal.mec@gmail.com>
2021-01-06 16:12:02 +0530

    Bottom Up Knapsack - resolved all errors

--- BottomUpKnapSack0-1/src/BottomUpKnapSack01.java
+++ BottomUpKnapSack0-1/src/BottomUpKnapSack01.java
@@ -6,28 +6,33 @@ public class BottomUpKnapSack01 {
     public static void main(String[] args) {
         int weight[] ={1, 3, 4, 5}, val[]= {1, 4, 5, 7}, W = 7;
         int len = val.length;
-        System.out.println(knapsack(weight, val, W, len));
+        int max = knapsack(weight, val, W, len);
+        System.out.println(max);
     }
 
     public static int knapsack(int[] weight, int[] val, int W, int len) {
         int[][] memo = new int[len+1][W+1];
+        int i=0, j=0;
         //base condition
-        for(int i=0; i<len+1; i++) {
-            for(int j=0; j< W+1; j++) {
+        for(i=0; i<len+1; i++) {
+            for(j=0; j< W+1; j++) {
                 if(i==0 || j==0)
                     memo[i][j] = 0;
             }
         }
 
         //choice diagram conditions
-        for(int i=1; i<len+1; i++) {
-            for(int j=1; j<W+1; j++) {
-                if(weight[i-1]>W)
-                    memo[i][j] = memo[i+1][j];
+        for(i=1; i<=len; i++) {
+            for(j=1; j<=W; j++) {
+                if(weight[i-1]<=j)
+                    memo[i][j] = Math.max(val[i-1] + memo[i-1][j-weight[i-1]], memo[i-1][j]);
+
                 else {
-                    memo[i][j] = Math.max();
+                    memo[i][j] = memo[i-1][j];
                 }
             }
         }
+
+        return memo[len][W];
     }
 }
