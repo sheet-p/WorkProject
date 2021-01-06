0f598f3cbd316544f2c3f5af53a009d85ccf6af3
sheet-p <sheetal.mec@gmail.com>
2021-01-06 21:44:23 +0530

    odd equal check done

--- EqualSumPartition/src/EqualSumPartition.java
+++ EqualSumPartition/src/EqualSumPartition.java
@@ -6,4 +6,21 @@ arr[] = {1, 5, 11, 5}
 Output: true
 The array can be partitioned as {1, 5, 5} and {11} */
 public class EqualSumPartition {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 11, 5}, sum = 0;
+        for(int i=0; i<arr.length; i++)
+            sum += arr[i];
+        /*arr can be partitioned into two only if sum is even
+            sum of partition 1 = S
+            sum of partition 2 = S
+            sum of full arr = 2S (which is even)  */
+        if(sum % 2 != 0)
+            System.out.println("False");
+        else
+            System.out.println(knapsack(arr, arr.length-1, sum/2));
+    }
+
+    private static boolean knapsack(int[] arr, int n, int sum) {
+
+    }
 }
