dcaf07b18c71121c83a36a6db8673511708247cb
sheet-p <sheetal.mec@gmail.com>
2021-01-06 21:36:54 +0530

    Problem statement of equal sum partition problem

---
+++ EqualSumPartition/src/EqualSumPartition.java
@@ -0,0 +1,9 @@
+/*Equal Sum Partition Problem
+Partition problem is to determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is same.
+Examples:
+
+arr[] = {1, 5, 11, 5}
+Output: true
+The array can be partitioned as {1, 5, 5} and {11} */
+public class EqualSumPartition {
+}
