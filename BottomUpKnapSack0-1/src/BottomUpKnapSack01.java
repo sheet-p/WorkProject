fc6996ba67d84ed800ea9eb7bb38963b6f06ea0c
sheet-p <sheetal.mec@gmail.com>
2021-01-06 14:35:51 +0530

    Bottom Up Knapsack starting

---
+++ BottomUpKnapSack0-1/src/BottomUpKnapSack01.java
@@ -0,0 +1,11 @@
+public class BottomUpKnapSack01 {
+    /* This is a 0-1 Knapsack problem. We are given a knapsack and its total weight capacity. We need to figure the
+    number of items to be put into this knapsack so that the profit is maximised. As input, we will be given the total
+     weight capacity of the knapsack, the weights of individual items, the value of those individual items.
+     */
+    public static void main(String[] args) {
+        int weight[] ={1, 3, 4, 5}, val[]= {1, 4, 5, 7}, W = 7;
+        int len = val.length;
+
+    }
+}
