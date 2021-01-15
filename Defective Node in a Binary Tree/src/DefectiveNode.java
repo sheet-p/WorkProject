d88e0a205c7ad9e6d222124fd3668ab1887cc9b9
sheet-p <sheetal.mec@gmail.com>
2021-01-15 21:14:24 +0530

    Defective Node in a binary tree

---
+++ Defective Node in a Binary Tree/src/DefectiveNode.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DefectiveNode {
+    public static void main(String[] args) {
+        Tree root = new Tree(3);
+        root.left = new Tree(7);
+        root.left.left = new Tree(4);
+        root.left.right = new Tree(2);
+        root.left.left.left = new Tree(1);
+        root.left.right.left = root.left.left.left;
+        //keeping track of visited
+        Set<Tree> visited = new HashSet<>();
+        System.out.println(isDefective(root, visited));
+    }
+
+    private static boolean isDefective(Tree root, Set<Tree> visited) {
+        if(root == null)
+            return ;
+        if(! visited.add(root))
+            return false;
+        visited.add(root);
+        isDefective(root.left, visited);
+        isDefective(root.right, visited);
+        return true;
+    }
+}
