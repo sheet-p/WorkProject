5fe5d82e741d2f9a221f9f7fd04d6c46dc7d296c
sheet-p <sheetal.mec@gmail.com>
2021-01-16 20:49:36 +0530

    Description added

---
+++ Queue Reconstruction/src/QueueReconstruction.java
@@ -0,0 +1,11 @@
+public class QueueReconstruction {
+    //LeetCode 406 :You are given an array of people, people, which are the attributes of some people in a queue
+    // (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly
+    // ki other people in front who have a height greater than or equal to hi.
+    //
+    //Reconstruct and return the queue that is represented by the input array people. The returned queue should be
+    // formatted as an array queue that satisfies the conditions/attributes
+    public static void main(String[] args) {
+        int[][] people = {{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
+    }
+}
