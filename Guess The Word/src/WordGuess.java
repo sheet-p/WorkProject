8ace94dc43a3bce619fa5778d6e297481942762e
sheet-p <sheetal.mec@gmail.com>
2021-01-21 14:13:52 +0530

    added implementation

---
+++ Guess The Word/src/WordGuess.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+//LeetCode 843
+public class WordGuess {
+    public static void main(String[] args) {
+        String secret = "hbaczn", wordlist[] = {"gaxckt","trlccr","jxwhkz","ycbfps","peayuf","yiejjw","ldzccp","nqsjoa","qrjasy","pcldos","acrtag","buyeia","ubmtpj","drtclz","zqderp","snywek","caoztp","ibpghw","evtkhl","bhpfla","ymqhxk","qkvipb","tvmued","rvbass","axeasm","qolsjg","roswcb","vdjgxx","bugbyv","zipjpc","tamszl","osdifo","dvxlxm","iwmyfb","wmnwhe","hslnop","nkrfwn","puvgve","rqsqpq","jwoswl","tittgf","evqsqe","aishiv","pmwovj","sorbte","hbaczn","coifed","hrctvp","vkytbw","dizcxz","arabol","uywurk","ppywdo","resfls","tmoliy","etriev","oanvlx","wcsnzy","loufkw","onnwcy","novblw","mtxgwe","rgrdbt","ckolob","kxnflb","phonmg","egcdab","cykndr","lkzobv","ifwmwp","jqmbib","mypnvf","lnrgnj","clijwa","kiioqr","syzebr","rqsmhg","sczjmz","hsdjfp","mjcgvm","ajotcx","olgnfv","mjyjxj","wzgbmg","lpcnbj","yjjlwn","blrogv","bdplzs","oxblph","twejel","rupapy","euwrrz","apiqzu","ydcroj","ldvzgq","zailgu","xgqpsr","wxdyho","alrplq","brklfk"};
+        Random random = new Random();
+        for(int i = 0, matches = 0; i < 10 && matches != 6; i ++){
+            String guess = wordlist[random.nextInt(wordlist.length)];
+            matches = master.guess(guess);
+            List<String> candidates = new ArrayList<>();
+            for(String word: wordlist){
+                if(matches == getMatches(guess, word)){
+                    candidates.add(word);
+                }
+            }
+
+            wordlist = candidates.toArray(new String[0]);
+        }
+    }
+
+    private static int getMatches(String word1, String word2){
+        int matches = 0;
+        for(int i = 0; i < word1.length(); i ++){
+            if(word1.charAt(i) == word2.charAt(i)){
+                matches ++;
+            }
+        }
+
+        return matches;
+    }
+}
