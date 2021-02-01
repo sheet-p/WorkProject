fbc717b1b260231dfdcb0735e253b44317924fd9
sheet-p <sheetal.mec@gmail.com>
2021-02-01 23:27:51 +0530

    implementation : largest time for given digits

---
+++ Largest Time for Given Digits/src/LargestTime.java
@@ -0,0 +1,21 @@
+public class LargestTime {
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,5};
+        int i = 0, j = 0, k = 0;
+        String result = "";
+        for(i=0; i<4; i++) {
+            for(j=0; j<4; j++) {
+                for(k=0; k<4; k++) {
+                    if(i==j || j==k || i==k)
+                        continue;
+                    String h = "" + arr[i] + arr[j];
+                    String m = "" + arr[k] + arr[6-i-j-k];
+                    String t = ""+h+":"+m;
+                    if((h.compareTo("24"))<0 && (m.compareTo("59"))<0 && result.compareTo(t)<0 )
+                        result = t;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
