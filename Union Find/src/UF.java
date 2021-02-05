05a0aafec1ad32b805fc450253def2b579819dc1
I503048 <sheetal.paul@sap.com>
2021-02-05 23:41:16 +0530

    description : added Kruskals

---
+++ Union Find/src/UF.java
@@ -0,0 +1,14 @@
+public class UF {
+    public static void main(String[] args) {
+        /* Let us create the following graph
+        0
+        | \
+        |  \
+        1---2----3 */
+        int V=4, E=4;
+        int[][] graph = { {0,1,1},
+                           {}
+
+        }
+    }
+}
