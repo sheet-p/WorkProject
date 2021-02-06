cb30727563d91ca6805fcdfb7f054168e283ecd6
sheet-p <sheetal.mec@gmail.com>
2021-02-06 10:21:33 +0530

    implementation : main added

--- Union Find/src/UF.java
+++ Union Find/src/UF.java
@@ -6,9 +6,14 @@ public class UF {
         |  \
         1---2----3 */
         int V=4, E=4;
-        int[][] graph = { {0,1,1},
-                           {};
-
-        }
+        int[][] graph = {  {0, 1, 1, 0},
+                           {0, 0, 1, 0},
+                           {0, 0, 0, 1} ,
+                           {0, 0, 0, 0} };
+        Grapg g = new Graph(V, E);
+        if(isCycle(g))
+            System.out.println("Cycle Exists!");
+        else
+            System.out.println("No Cycle!");
     }
 }
