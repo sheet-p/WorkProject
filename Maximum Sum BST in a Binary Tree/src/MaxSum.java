5967e42422d2e313b3b5ca12c3adb62279ab3b1d
sheet-p <sheetal.mec@gmail.com>
2021-02-14 21:20:12 +0530

    implementation : main added

--- Maximum Sum BST in a Binary Tree/src/MaxSum.java
+++ Maximum Sum BST in a Binary Tree/src/MaxSum.java
@@ -1,7 +1,26 @@
 //https://leetcode.com/problems/maximum-sum-bst-in-binary-tree/submissions/
 public class MaxSum {
+    static int max;
     Tree root;
     MaxSum() {
         root = null;
+    }
+
+    public static void main(String[] args) {
+
+        MaxSum tree = new MaxSum();
+        tree.root = new Tree(1);
+        tree.root.left = new Tree(2);
+        tree.root.right = new Tree(3);
+        tree.root.left.left = new Tree(4);
+        tree.root.left.right = new Tree(5);
+
+        max = 0;
+        findMax(tree.root);
+        System.out.println(max);
+    }
+
+    private static int[] findMax(Tree root) {
+
     }
 }
