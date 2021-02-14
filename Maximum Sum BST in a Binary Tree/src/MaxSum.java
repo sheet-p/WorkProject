0043e0107d0ee67fa15df1fa8eab1e35f27f8ffd
sheet-p <sheetal.mec@gmail.com>
2021-02-14 23:25:19 +0530

    implementation : logic added

--- Maximum Sum BST in a Binary Tree/src/MaxSum.java
+++ Maximum Sum BST in a Binary Tree/src/MaxSum.java
@@ -20,7 +20,21 @@ public class MaxSum {
         System.out.println(max);
     }
 
+    //int[] {1/0 --> BST or not, largest, smallest, sum}
     private static int[] findMax(Tree root) {
+        if(root == null)
+            return new int[]{1, Integer.MIN_VALUE, Integer.MAX_VALUE, 0};
 
+        int[] left = findMax(root.left);
+        int[] right = findMax(root.right);
+
+        boolean isBST = left[0]==1 && right[0]==1 && (root.data>left[1]) && (root.data < right[2]);
+        int sum = root.data + left[3] + right[3];
+
+        if(isBST) {
+            max = Math.max(max, sum);
+        }
+
+        return new int[]{isBST?1:0, Math.max(root.data, right[1]), Math.min(root.data, left[2]), sum};
     }
 }
