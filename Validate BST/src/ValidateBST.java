cd94aad1a74a07744abe5a88d8991d5968edf362
sheet-p <sheetal.mec@gmail.com>
2021-02-14 20:49:50 +0530

    implementation : validate a BST

---
+++ Validate BST/src/ValidateBST.java
@@ -0,0 +1,25 @@
+public class ValidateBST {
+    Tree root;
+    ValidateBST() {
+        root = null;
+    }
+
+    public static void main(String[] args) {
+        ValidateBST tree = new ValidateBST();
+        tree.root = new Tree(1);
+        tree.root.left = new Tree(2);
+        tree.root.right = new Tree(3);
+        tree.root.left.left = new Tree(4);
+        tree.root.left.right = new Tree(5);
+
+        System.out.println(isBST(tree.root, -1, -1));
+    }
+
+    private static boolean isBST(Tree root, int min, int max) {
+        if(root == null)
+            return true;
+        if( (min!=-1 && root.data<=min) && (max!=-1 && root.data>=max) )
+            return false;
+        return isBST(root.left, min, root.data) && isBST(root.right, root.data, max);
+    }
+}
