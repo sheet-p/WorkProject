6725ad748aab12e2cd1585d4f0ceb20671482d83
sheet-p <sheetal.mec@gmail.com>
2021-02-14 21:17:35 +0530

    skeleton : max sum BST in a BT

---
+++ Maximum Sum BST in a Binary Tree/src/MaxSum.java
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/maximum-sum-bst-in-binary-tree/submissions/
+public class MaxSum {
+    Tree root;
+    MaxSum() {
+        root = null;
+    }
+}
---
+++ Maximum Sum BST in a Binary Tree/src/Tree.java
@@ -0,0 +1,10 @@
+public class Tree {
+    int data;
+    Tree left, right;
+
+    Tree(int value) {
+        this.data = value;
+        this.left = null;
+        this.right = null;
+    }
+}
