e773315f0fc9e52ac8f9779866fd0f2e512d65bd
sheet-p <sheetal.mec@gmail.com>
2021-02-17 20:06:21 +0530

    implementation : coin change

---
+++ Coin Change - Max Number of Ways/src/CoinChange.java
@@ -0,0 +1,18 @@
+//Given a value N, if we want to make change for N cents, and we have infinite supply of each of
+// S = { S1, S2, .. , Sm} valued coins, how many ways can we make the change? The order of coins doesn’t matter.
+//Example:
+//for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}. So output should be 4.
+public class CoinChange {
+    public static void main(String[] args) {
+        int coin[]={1,2,3}, N=4;
+        System.out.println(change(coin, coin.length, N));
+    }
+
+    private static int change(int[] coin, int n, int sum) {
+        if(sum==0)
+            return 1;
+        if(n==0)
+            return 0;
+        return change(coin, n, sum-coin[n]) + change(coin, n-1, sum);
+    }
+}
