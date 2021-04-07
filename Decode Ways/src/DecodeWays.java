71c22edb63ed3d675416e4945df86bc8cdd52da9
sheet-p <sheetal.mec@gmail.com>
2021-04-07 16:08:53 +0530

    implementation : added comments

--- Decode Ways/src/DecodeWays.java
+++ Decode Ways/src/DecodeWays.java
@@ -23,12 +23,16 @@ public class DecodeWays {
         if(index==s.length() || index==s.length()-1)
             return 1;
 
+        //if it starts with 0 (eg. 06) then its invalid
         if(s.charAt(index) == '0')
             return 0;
 
         if(memo[index] != -1)
             return memo[index];
 
+        //it can either be decoded by one char or 2 characters if both characters come upto less than 26
+        //eg 112 -> 1+decode(12) or 11+decode(2)
+        //eg 272 -> 2+decode(72)
         int result = decode(s, index+1);
         if(Integer.parseInt(s.substring(index, index+2)) <= 26)
             result += decode(s, index+2);
