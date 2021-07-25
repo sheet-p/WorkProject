892737f19e85723cd6e26cfab244cc7055cd8109
sheet-p <sheetal.mec@gmail.com>
2021-07-25 12:37:02 +0530

    implementation : check if a bst is valid

---
+++ Check if BST is Valid/src/Tree.java
@@ -0,0 +1,10 @@
+public class Tree {
+    int data;
+    Tree left, right;
+
+    Tree(int v) {
+        this.data = v;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ Check if BST is Valid/src/isValid.java
@@ -0,0 +1,22 @@
+public class isValid {
+    static Tree root;
+
+    public static void main(String[] args) {
+        root = new Tree(5);
+        root.left = new Tree(1);
+        root.right = new Tree(4);
+        root.right.left = new Tree(3);
+        root.right.right = new Tree(6);
+
+        System.out.println(isValidBST(root, null, null));
+    }
+
+    private static boolean isValidBST(Tree root, Integer min, Integer max) {
+        if(root == null)
+            return true;
+        if( (min!=null && root.data<= min) || (max!=null && root.data >= max) )
+            return false;
+        return ((isValidBST(root.left, min, root.data)) && (isValidBST(root.right, root.data, max)) );
+
+    }
+}
